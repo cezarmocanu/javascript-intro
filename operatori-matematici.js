8eee75c84ef61caf3cac844ceb07d5398691ab3b
cezarmocanu <cezarmocanu2014@gmail.com>
2023-03-09 21:15:29 +0200

    operatori matematici

---
+++ operatori-matematici.js
@@ -0,0 +1,18 @@
+const a = 91;
+const b = 2;
+
+const suma = a + b;
+const diferenta = a - b;
+const produs = a * b;
+const cat = a / b;
+const rest = a % b;//modulo / mod
+
+//impartirea intreaga
+const catulIntreg = Math.floor(a / b);//div
+
+console.log(suma);
+console.log(diferenta);
+console.log(produs);
+console.log(cat);
+console.log(rest);
+console.log(catulIntreg);
