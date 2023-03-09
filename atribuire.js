cb0c735a58bd1f5b87273d0cec1e6f940a24a4ac
cezarmocanu <cezarmocanu2014@gmail.com>
2023-03-09 21:29:16 +0200

    Atribuire

---
+++ atribuire.js
@@ -0,0 +1,33 @@
+let a = 10;
+console.log(a); 
+
+//Incrementare cu 50
+a = a + 50;
+console.log(a); 
+
+//Incrementare cu 50
+a += 50;
+//Mai exista si -=, *=, /=, %=
+console.log(a); 
+
+//Incrementare cu 1 (STRICT CU 1)
+a++;
+// Mai exista si a--;
+console.log(a);
+
+//Echivalente
+// a = a + 50;
+// a += 50;
+
+// a = a - 50;
+// a -= 50;
+
+// a = a * 50;
+// a *= 50;
+
+// a = a / 50;
+// a /= 50;
+
+// a = a % 50;
+// a %= 50;
+
