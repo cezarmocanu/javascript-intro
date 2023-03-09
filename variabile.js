b966a0878adf105b57aa620091642c0bc38dc408
cezarmocanu <cezarmocanu2014@gmail.com>
2023-03-09 19:52:15 +0200

    Script pentru variabile

---
+++ variabile.js
@@ -0,0 +1,18 @@
+//declaram variabilele
+const variabilaImutabila = 50;
+let variabilaMutabila = 25;
+
+//citim
+console.log(variabilaImutabila); // 50
+
+// Ne va afisa o eroare fiindca incercam
+// sa schimbam valoarea dintr-o variabila imutabila/constanta
+// variabilaImutabila = 10;
+// console.log(variabilaImutabila);
+
+console.log(variabilaMutabila); // 25
+
+//modificam/reatribuim
+variabilaMutabila = 10;
+
+console.log(variabilaMutabila); // 10
