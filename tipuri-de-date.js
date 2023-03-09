6de9f8f4e5147d2fa7b0b7fc04f0db6346a00a8b
cezarmocanu <cezarmocanu2014@gmail.com>
2023-03-09 20:06:37 +0200

    Tipuri de variabile primitive

---
+++ tipuri-de-date.js
@@ -0,0 +1,19 @@
+//Liste, Functii, Obiecte, Dictionare, null, Map, Set - TIPURI DE DATE PRIN REFERINTA
+
+//PRIMITIVE - TIPURI DE DATE PRIMITIVE
+// undefined - pentur ca nu i s-a dat o valoare
+let euSuntDoarDeclarat;
+
+// number - pentru numere intregi(15) si cu virgula(15.8)
+let box = 15.8;
+
+// string/text - pentru texte intre ghilimele
+let text = "Salutare Luna";
+
+// boolean - pentru true sau false
+let amTricouRosu = false;
+
+console.log(euSuntDoarDeclarat);
+console.log(box);
+console.log(text);
+console.log(amTricouRosu);
